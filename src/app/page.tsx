a98c5d4bede970d765f968778f6076629f4ec68e
CarlosDayan <dayaneishon12@gmail.com>
2026-03-24 13:33:47 -0600

    step 1: creating the proyect

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center font-sans">
+      <h1 className="text-6xl font-bold tracking-tighter mb-4">
+        Accord
+      </h1>
+      <p className="text-xl text-gray-400">
+        El sistema operativo de tu tiempo.
+      </p>
+    </div>
+  );
+}
